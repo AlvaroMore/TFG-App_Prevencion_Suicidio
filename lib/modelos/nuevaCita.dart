0c12fbdfc02484c6faec5bcb7365d7430423fa4e
AlvaroMore <80346525+AlvaroMore@users.noreply.github.com>
2023-06-14 23:56:15 +0200

    Editar y eliminar citas. Usuarios y administrador

--- lib/modelos/nuevaCita.dart
+++ lib/modelos/nuevaCita.dart
@@ -4,8 +4,14 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:demo/modelos/cita.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class NuevaCita extends StatefulWidget {
+  final int? citaIndex;
+  final List<Appointment> appointments;
+
+  NuevaCita({this.citaIndex, required this.appointments});
+
   @override
   NuevaCitaState createState() => NuevaCitaState();
 }
@@ -14,31 +20,62 @@ class NuevaCitaState extends State<NuevaCita> {
   final _database = FirebaseDatabase.instance;
   final TextEditingController tituloController = TextEditingController();
   final user = FirebaseAuth.instance.currentUser;
-  DateTime fechaInicio = DateTime.now();
-  DateTime fechaFin = DateTime.now();
+  late DateTime fechaInicio;
+  late DateTime fechaFin;
   DateFormat dateFormat = DateFormat('dd-MM-yyyy');
   DateFormat timeFormat = DateFormat('HH:mm');
 
+  @override
+  void initState() {
+    super.initState();
+    if (widget.citaIndex != null) {
+      Appointment appointment = widget.appointments[widget.citaIndex!];
+      fechaInicio = appointment.startTime;
+      fechaFin = appointment.endTime;
+      tituloController.text = appointment.subject;
+    } else {
+      fechaInicio = DateTime.now();
+      fechaFin = DateTime.now();
+    }
+  }
+
   void guardarCita() {
     String titulo = tituloController.text.trim();
-    Cita cita = Cita(
-      titulo: titulo,
-      fechaInicio: fechaInicio,
-      fechaFin: fechaFin,
-      userId: user?.uid ?? '',
+    Appointment appointment = Appointment(
+      startTime: fechaInicio,
+      endTime: fechaFin,
+      subject: titulo,
+      color: Colors.blue,
     );
-    
+
     var rng = Random();
-    var key_ = rng.nextInt(10000);
-    final datosRef = _database.reference().child('citas/$key_');
+    var key_ = rng.nextInt(10000).toString();
 
-    datosRef.set({
-      "Titulo": cita.titulo,
-      "FechaInicio": cita.fechaInicio.toString(),
-      "FechaFin": cita.fechaFin.toString(),
-      "UserId": cita.userId,
-    }).asStream();
+    if (widget.citaIndex != null) {
+      Appointment citaPulsada = widget.appointments[widget.citaIndex!];
+      DatabaseReference citaOriginal = _database.reference().child('citas/${citaPulsada.id}');
+      citaOriginal.remove();
 
+      DatabaseReference citaEditada = _database.reference().child('citas/$key_');
+      citaEditada.set({
+        "Titulo": appointment.subject,
+        "FechaInicio": appointment.startTime.toString(),
+        "FechaFin": appointment.endTime.toString(),
+        "UserId": user?.uid ?? '',
+      }).asStream();
+      citaPulsada.subject = appointment.subject;
+      citaPulsada.startTime = appointment.startTime;
+      citaPulsada.endTime = appointment.endTime;
+    } else {
+      DatabaseReference datosRef = _database.reference().child('citas/$key_');
+      datosRef.set({
+        "Titulo": appointment.subject,
+        "FechaInicio": appointment.startTime.toString(),
+        "FechaFin": appointment.endTime.toString(),
+        "UserId": user?.uid ?? '',
+      }).asStream();
+      widget.appointments.add(appointment);
+    }
     Navigator.pop(context);
   }
 
@@ -46,7 +83,7 @@ class NuevaCitaState extends State<NuevaCita> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Nueva Cita'),
+        title: Text(widget.citaIndex != null ? 'Editar Cita' : 'Nueva Cita'),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
@@ -92,7 +129,7 @@ class NuevaCitaState extends State<NuevaCita> {
                     onTap: () async {
                       TimeOfDay? pickedTime = await showTimePicker(
                         context: context,
-                        initialTime: TimeOfDay.now(),
+                        initialTime: TimeOfDay.fromDateTime(fechaInicio),
                       );
                       if (pickedTime != null) {
                         setState(() {
@@ -150,7 +187,7 @@ class NuevaCitaState extends State<NuevaCita> {
                     onTap: () async {
                       TimeOfDay? pickedTime = await showTimePicker(
                         context: context,
-                        initialTime: TimeOfDay.now(),
+                        initialTime: TimeOfDay.fromDateTime(fechaFin),
                       );
                       if (pickedTime != null) {
                         setState(() {
@@ -185,5 +222,7 @@ class NuevaCitaState extends State<NuevaCita> {
     );
   }
 }
+
+
 
 
--- lib/paginas/calendario.dart
+++ lib/paginas/calendario.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:demo/modelos/nuevaCita.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
+import 'package:demo/modelos/cita.dart';
 
 class Calendario extends StatefulWidget {
   @override
@@ -13,39 +15,95 @@ class DataSource extends CalendarDataSource {
   DataSource(List<Appointment> source) {
     appointments = source;
   }
+
+  @override
+  String citaId(int index) {
+    return appointments![index].id;
+  }
+
+  void actualizarCita(List<Appointment> updatedAppointments) {
+    appointments = updatedAppointments;
+    notifyListeners(CalendarDataSourceAction.reset, []);
+  }
 }
 
 class CalendarioState extends State<Calendario> {
-  List<Appointment> appointment= <Appointment>[];
-  final DatabaseReference baseDatos = FirebaseDatabase.instance.reference();
+  List<Appointment> appointments = <Appointment>[];
+  final baseDatos = FirebaseDatabase.instance;
+  bool datosCargados = false;
+  String? citaSeleccionadaId;
+  var userRole;
 
   @override
   void initState() {
     super.initState();
+    if (!datosCargados) {
+      fetchUserRoleAndCargaCitas();
+      datosCargados = true;
+    }
+  }
+
+  void fetchUserRoleAndCargaCitas() async {
+    await fetchUserRole();
     cargaCitas();
   }
 
-  void cargaCitas() {
-    final citas = baseDatos.child('citas');
+  Future<String> getUserRole(String userId) async {
+    final userRoleRef = baseDatos.reference().child('users/$userId/rol');
+    DatabaseEvent snapshot = await userRoleRef.once();
+    var snapshotValue = snapshot.snapshot.value;
+    return (snapshotValue as String?) ?? '';
+  }
 
+  Future<void> fetchUserRole() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      String role = await getUserRole(user.uid);
+      setState(() {
+        userRole = role;
+      });
+    }
+  }
+
+  void cargaCitas() {
+    final citas = baseDatos.reference().child('citas');
     citas.onChildAdded.listen((event) {
       Map<dynamic, dynamic> value = event.snapshot.value as Map<dynamic, dynamic>;
-
       if (value != null) {
+        String? id = event.snapshot.key!;
         String titulo = value['Titulo'];
         String userId = value['UserId'];
         String fechaInicio = value['FechaInicio'];
         String fechaFin = value['FechaFin'];
         DateTime startTime = DateTime.parse(fechaInicio);
         DateTime endTime = DateTime.parse(fechaFin);
-        appointment.add(Appointment(
-          startTime: startTime,
-          endTime: endTime,
-          subject: titulo,
-          color: Colors.blue,
-        ));
+        if (userRole == "administrador" || userId == FirebaseAuth.instance.currentUser?.uid) {
+          bool isDuplicate = appointments.any((appointment) {
+            return appointment.subject == titulo &&
+                appointment.startTime == startTime &&
+                appointment.endTime == endTime;
+          });
+          if (!isDuplicate) {
+            appointments.add(Appointment(
+              id: id,
+              startTime: startTime,
+              endTime: endTime,
+              subject: titulo,
+              color: Colors.blue,
+            ));
+            DataSource dataSource = DataSource(appointments);
+            dataSource.actualizarCita(appointments);
+          }
+        }
         setState(() {});
       }
+    });
+    citas.onChildRemoved.listen((event) {
+      String? id = event.snapshot.key;
+      appointments.removeWhere((appointment) => appointment.id == id);
+      DataSource dataSource = DataSource(appointments);
+      dataSource.actualizarCita(appointments);
+      setState(() {});
     });
   }
 
@@ -61,17 +119,17 @@ class CalendarioState extends State<Calendario> {
         dataSource: listaCitas(),
         onTap: (CalendarTapDetails details) {
           if (details.targetElement == CalendarElement.calendarCell) {
-            DateTime tappedDate = details.date!;
-            List<Appointment> tappedAppointments = citasFecha(tappedDate);
+            DateTime diaPulsado = details.date!;
+            List<Appointment> citaPulsada = citasFecha(diaPulsado);
 
             showDialog(
               context: context,
               builder: (context) {
                 return AlertDialog(
-                  title: Text(formatoFecha(tappedDate)),
+                  title: Text(formatoFecha(diaPulsado)),
                   content: Column(
                     mainAxisSize: MainAxisSize.min,
-                    children: mostrarCitas(tappedAppointments),
+                    children: mostrarCitas(context, citaPulsada),
                   ),
                   actions: [
                     TextButton(
@@ -84,6 +142,18 @@ class CalendarioState extends State<Calendario> {
                 );
               },
             );
+          } else if (details.targetElement == CalendarElement.appointment) {
+            Appointment appointment = details.appointments![0];
+            citaSeleccionadaId = appointment.id as String?;
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => NuevaCita(
+                  appointments: appointments,
+                  citaIndex: appointments.indexOf(appointment),
+                ),
+              ),
+            );
           }
         },
       ),
@@ -91,14 +161,16 @@ class CalendarioState extends State<Calendario> {
         onPressed: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (_) => NuevaCita()),
+            MaterialPageRoute(
+              builder: (_) => NuevaCita(appointments: appointments),
+            ),
           );
         },
         child: Icon(Icons.add),
       ),
     );
   }
-  
+
   String formatoFecha(DateTime date) {
     return DateFormat('dd MMMM, yyyy').format(date);
   }
@@ -108,39 +180,81 @@ class CalendarioState extends State<Calendario> {
   }
 
   DataSource listaCitas() {
-    return DataSource(appointment);
+    return DataSource(appointments);
   }
 
   List<Appointment> citasFecha(DateTime date) {
-    return appointment.where((appointment) {
+    return appointments.where((appointment) {
       DateTime appointmentStartDate = appointment.startTime;
       DateTime appointmentEndDate = appointment.endTime;
-      
-      // Check if the date falls within the appointment period
-      return date.isAfter(appointmentStartDate.subtract(Duration(days: 1))) &&
-          date.isBefore(appointmentEndDate.add(Duration(days: 1)));
+      DateTime startDate = DateTime(appointmentStartDate.year, appointmentStartDate.month, appointmentStartDate.day);
+      DateTime endDate = DateTime(appointmentEndDate.year, appointmentEndDate.month, appointmentEndDate.day);
+
+      if (startDate == endDate) {
+        return date.year == startDate.year && date.month == startDate.month && date.day == startDate.day;
+      } else {
+        return date.isAfter(startDate.subtract(Duration(days: 1))) && date.isBefore(endDate.add(Duration(days: 1)));
+      }
     }).toList();
   }
 
-  List<Widget> mostrarCitas(List<Appointment> appointments) {
+  List<Widget> mostrarCitas(BuildContext context, List<Appointment> appointments) {
     if (appointments.isEmpty) {
       return [Text('No hay citas')];
     } else {
       return appointments.map((appointment) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Titulo: ${appointment.subject}'),
-            Text('Inicio: ${formatoFechaTiempo(appointment.startTime)}'),
-            Text('Fin: ${formatoFechaTiempo(appointment.endTime)}'),
-            SizedBox(height: 16),
-          ],
+        return GestureDetector(
+          onTap: () {
+            citaSeleccionadaId = appointment.id as String?;
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => NuevaCita(
+                  appointments: appointments,
+                  citaIndex: appointments.indexOf(appointment),
+                ),
+              ),
+            );
+          },
+          child: Container(
+            margin: EdgeInsets.only(bottom: 16.0),
+            padding: EdgeInsets.all(8.0),
+            color: Colors.grey[300],
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Titulo: ${appointment.subject}'),
+                      Text('Inicio: ${formatoFechaTiempo(appointment.startTime)}'),
+                      Text('Fin: ${formatoFechaTiempo(appointment.endTime)}'),
+                    ],
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    eliminarCita(appointment.id! as String);
+                    appointments.remove(appointment);
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.delete, color: Colors.red),
+                ),
+              ],
+            ),
+          ),
         );
       }).toList();
     }
   }
 
+  void eliminarCita(String appointmentId) {
+    baseDatos.reference().child('citas').child(appointmentId).remove();
+  }
 }
+
+
+
 
 
 
