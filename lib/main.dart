d9117835b44bc59edee8c7d2ab261679e86ee724
AlvaroMore <80346525+AlvaroMore@users.noreply.github.com>
2023-07-05 00:12:11 +0200

    Notificaciones Notas

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:demo/paginas/calendario.dart';
 import 'package:demo/paginas/enlace.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> backgroundHandler(RemoteMessage message) async{
+  String? title = message.notification!.title;
+  String? body = message.notification!.body;
+
+}
 
 Future<void> main() async{
+  
+
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
--- lib/modelos/nuevaNota.dart
+++ lib/modelos/nuevaNota.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:math';
 import 'package:demo/paginas/notas.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -5,6 +6,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:demo/modelos/nota.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:http/http.dart' as http;
 
 class NuevaNota extends StatefulWidget {
   @override
@@ -16,6 +18,44 @@ class NuevaNotaState extends State<NuevaNota> {
   TextEditingController contenido = TextEditingController();
 
   final baseDatos = FirebaseDatabase.instance;
+  String? adminToken;
+
+  @override
+  initState() {
+    super.initState();
+    fetchAdminToken();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      String? title = message.notification!.title;
+      String? body = message.notification!.body;
+      // Aquí puedes realizar alguna acción con la notificación recibida
+    });
+  }
+
+  Future<void> fetchAdminToken() async {
+    final usersRef = baseDatos.reference().child('users');
+    final DatabaseEvent dataSnapshot = await usersRef.once();
+    
+    final usuarios = dataSnapshot.snapshot.value as Map<dynamic, dynamic>;
+
+    String? adminUserId;
+    
+    // Buscar el usuario con el rol de administrador
+    usuarios.forEach((userId, userData) {
+      final rol = userData['rol'] as String?;
+      if (rol == 'administrador') {
+        adminUserId = userId;
+        return;
+      }
+    });
+
+    if (adminUserId != null) {
+      final adminTokenRef = baseDatos.reference().child('users/$adminUserId/token');
+      final DatabaseEvent tokenSnapshot = await adminTokenRef.once();
+      adminToken = tokenSnapshot.snapshot.value as String?;
+    }
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,11 +70,11 @@ class NuevaNotaState extends State<NuevaNota> {
         title: Text('Nueva Nota'),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
-          onPressed: (){
+          onPressed: () {
             Navigator.pushReplacement(
               context,
-              MaterialPageRoute(builder: (_) => Notas())
-              );
+              MaterialPageRoute(builder: (_) => Notas()),
+            );
           },
         ),
       ),
@@ -86,6 +126,7 @@ class NuevaNotaState extends State<NuevaNota> {
                   "FechaCreacion": nuevaNota.fechaCreacion.toString(),
                   "UserId": nuevaNota.userId,
                 }).asStream();
+                sendPushNotification();
                 Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => Notas()));
               },
               child: Text(
@@ -101,4 +142,35 @@ class NuevaNotaState extends State<NuevaNota> {
       ),
     );
   }
+
+  Future<void> sendPushNotification() async {
+    try {
+      http.Response response = await http.post(
+        Uri.parse('https://fcm.googleapis.com/fcm/send'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'key=AAAAwExjmR0:APA91bGDItwHFI6kNHxmcHM9cCrUwcEhKnjWOCYhllfHXiUZY-RklTRMr-ieHciAKvWiRoephqgNGtCaOwSQ896IZJOj2wce-_IM9oDSApNg6Xx_3f1hV8sIrj7aiTtprwX4VVVSIn6R',
+        },
+        body: jsonEncode(
+          <String, dynamic>{
+            'notification': <String, dynamic>{
+              'body': "Se ha creado una nueva nota",
+              'title': 'Nueva nota',
+            },
+            'priority': 'high',
+            'data': <String, dynamic>{
+              'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+              'id': '1',
+              'status': 'done'
+            },
+            'to': adminToken,
+          },
+        ),
+      );
+      response;
+    } catch (e) {
+      e;
+    }
+  }
 }
+
